f01e289523940473b25a720faed101f1fd88566b
Mavenn58 <chingnicolas58@gmail.com>
2024-10-03 02:33:25 +0800

    update process function

--- smi.cpp
+++ smi.cpp
@@ -7,29 +7,30 @@
 
 struct Process {
     int gpuId;
-    int graphicsId;
-    int computeId;
+    std::string graphicsId;
+    std::string computeId;
     int pid;
     std::string type;
     std::string name;
-    int gpuMemoryUsage;
+    std::string gpuMemoryUsage;
 };
 
 void gpuSummary()
 {
-    std::cout << "\n+----------------------------------------------------------------------------------------------------+\n";
-    std::cout <<"| NVIDIA-SMI 551.86                 DRIVER VERSION: 551.86             CUDA VERSION: 12.4            |\n";
-    std::cout <<"|--------------------------------+---------------------------+---------------------------------------|\n";
-    std::cout <<"| GPU   Name                    TCC/WDM       | Bus-Id              Disp.A | Volatile  Uncorr.   ECC |\n";
-    std::cout <<"| FAN   Temp     Perf           Pwr:Usage/Cap |               Memory-Usage | GPU-Util  Compute   M.  |\n";
-    std::cout <<"|                                             |                            |               MIG   M.  |\n";
-    std::cout <<"+================================+===========================+=======================================+\n";
-    std::cout <<"|   0   NVIDIA   GeForce GTX 1080       WDDM  |   00000000:26:00.0      On |                     N/A |\n";
-    std::cout <<"| 28%    37C      P8              11W /  180W |     701MiB /       8192MiB |      0%         Default |\n";
-    std::cout <<"|                                             |                            |                     N/A |\n";
-    std::cout <<"+----------------------------------------------------------------------------------------------------+";
-    std::cout <<"\n";
+    std::cout << "\n+------------------------------------------------------------------------------------------+\n";
+    std::cout << "| NVIDIA-SMI 551.86             DRIVER VERSION: 551.86             CUDA VERSION: 12.4      |\n";
+    std::cout << "|---------------------------------------------+------------------------+-------------------|\n";
+    std::cout << "| GPU   Name                 TCC/WDM    | Bus-Id          Disp.A | Volatile  Uncorr.   ECC |\n";
+    std::cout << "| FAN   Temp     Perf     Pwr:Usage/Cap |           Memory-Usage | GPU-Util  Compute   M.  |\n";
+    std::cout << "|                                       |                        |               MIG   M.  |\n";
+    std::cout << "+=======================================+========================+=========================+\n";
+    std::cout << "|   0  NVIDIA GeForce GTX 1080     WDDM |   00000000:26:00.0  On |                     N/A |\n";
+    std::cout << "| 28%   37C     P8          11W /  180W |     701MiB /   8192MiB |      0%         Default |\n";
+    std::cout << "|                                       |                        |                     N/A |\n";
+    std::cout << "+---------------------------------------+------------------------+-------------------------+\n";
+    std::cout << "\n";
 }
+
 
 void dateTime()
 {
@@ -41,17 +42,30 @@ void dateTime()
 
 void displayProcess(const std::vector<Process>& processes)
 {
-    std::cout << "\n+----------------------------------------------------------------------------------------------------+\n";
-    std::cout << "| Processes:                                                                                         |\n";
-    std::cout << "|  GPU   GI     CI       PID   Type   Process name                                        GPU Memory |\n";
-    std::cout << "|        ID     ID                                                                        Usage      |\n";
-    std::cout << "|----------------------------------------------------------------------------------------------------|\n";
+    std::cout << "\n+------------------------------------------------------------------------------------------+\n";
+    std::cout << "| Processes:                                                                               |\n";
+    std::cout << "|  GPU   GI       CI       PID   Type   Process name                            GPU Memory |\n";
+    std::cout << "|        ID       ID                                                            Usage      |\n";
+    std::cout << "|------------------------------------------------------------------------------------------|\n";
 
     for (const auto& process : processes) {
-        std::cout << "| " << std::setw(4) << process.gpuId << " " << std::setw(4) << process.graphicsId << " " << std::setw(4) << process.computeId << " " << std::setw(6) << process.pid << " " << std::setw(5) << process.type << " " << std::setw(40) << process.name << " " << std::setw(10) << process.gpuMemoryUsage << "MiB |\n";
+        std::string name = process.name;
+        std::string gpuMemoryUsage = process.gpuMemoryUsage;
+
+        // Limit the process name to 30 characters
+        if (name.length() > 30) {
+            name = name.substr(0, 30) + "...";
+        }
+
+        // Limit the GPU memory usage to 10 characters
+        if (gpuMemoryUsage.length() > 10) {
+            gpuMemoryUsage = gpuMemoryUsage.substr(0, 10) + "...";
+        }
+
+        std::cout << "|" << std::setw(4) << process.gpuId << " " << std::setw(8) << process.graphicsId << " " << std::setw(8) << process.computeId << " " << std::setw(6) << process.pid << " " << std::setw(5) << process.type << "    " << std::setw(40) << std::left << name << " " << std::setw(9) << std::right << gpuMemoryUsage << "|\n";
     }
 
-    std::cout << "+====================================================================================================+\n";
+    std::cout << "+==========================================================================================+\n";
     std::cout << "\n";
 }
 
@@ -62,9 +76,11 @@ int main()
     gpuSummary();
 
     std::vector<Process> processes = {
-        {0, 1, 2, 1234, "System", "System Process", 100},
-        {0, 3, 4, 5678, "User", "User Process", 200},
-        {0, 5, 6, 9012, "System", "System Process 2", 300}
+        {0, "N/A", "N/A", 1368, "C+G", "C:\\Program Files\\DataSync\\DataSyncService.exe", "N/A"},
+        {0, "N/A", "N/A", 2166, "C+G", "C:\\Program Files\\ImageProcessing\\AdvancedImageProcessingDaemonWithBatchSupport.exe", "N/A"},
+        {0, "N/A", "N/A", 4244, "C+G", "C:\\Program Files\\NetworkMonitor\\NetworkMonitorAgent.exe", "N/A"},
+        {0, "N/A", "N/A", 5684, "C+G", "C:\\Program Files\\UserAuth\\UserAuthManager.exe", "N/A"},
+        {0, "N/A", "N/A", 6676, "C+G", "C:\\Program Files\\SystemCleanup\\ThoroughSystemCleanupTaskWithSchedulingOptions.exe", "N/A"},
     };
 
     displayProcess(processes);
