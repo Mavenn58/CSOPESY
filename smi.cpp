37d971c4e14679377082fd5762db76d753ae31fa
Mavenn58 <chingnicolas58@gmail.com>
2024-10-02 08:58:20 +0800

    processes w/o vectors

--- smi.cpp
+++ smi.cpp
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <vector>
+#include <string>
 #include <iostream>
 #include <ctime>
 
 void gpuSummary()
 {
-    std::cout << "\n+---------------------------------------------------------------------------------------------------+\n";
+    std::cout << "\n+----------------------------------------------------------------------------------------------------+\n";
     std::cout <<"| NVIDIA-SMI 551.86                 DRIVER VERSION: 551.86             CUDA VERSION: 12.4            |\n";
     std::cout <<"|--------------------------------+---------------------------+---------------------------------------|\n";
     std::cout <<"| GPU   Name                    TCC/WDM       | Bus-Id              Disp.A | Volatile  Uncorr.   ECC |\n";
     std::cout <<"| FAN   Temp     Perf           Pwr:Usage/Cap |               Memory-Usage | GPU-Util  Compute   M.  |\n";
     std::cout <<"|                                             |                            |               MIG   M.  |\n";
     std::cout <<"+================================+===========================+=======================================+\n";
-    std::cout <<"|   0   NVIDIA   GeForce GTX 1080       WDDM  |                            |                         |\n";
-    std::cout <<"| 28%    37C      P8              11W /  180W |                            |                         |\n";
-    std::cout <<"|                                             |                            |                         |\n";
+    std::cout <<"|   0   NVIDIA   GeForce GTX 1080       WDDM  |   00000000:26:00.0      On |                     N/A |\n";
+    std::cout <<"| 28%    37C      P8              11W /  180W |     701MiB /       8192MiB |      0%         Default |\n";
+    std::cout <<"|                                             |                            |                     N/A |\n";
     std::cout <<"+----------------------------------------------------------------------------------------------------+";
     std::cout <<"\n";
 }
@@ -27,10 +28,25 @@ void dateTime()
     std::cout << ctime(&timestamp);
 }
 
+void displayProcess()
+{
+    std::cout << "\n+----------------------------------------------------------------------------------------------------+\n";
+    std::cout <<"| Processes:                                                                                         |\n";
+    std::cout <<"|  GPU   GI     CI       PID   Type   Process name                                        GPU Memory |\n";
+    std::cout <<"|        ID     ID                                                                        Usage      |\n";
+    std::cout <<"|----------------------------------------------------------------------------------------------------|\n";
+    std::cout <<"|                                                                                                    |\n";
+    std::cout <<"|                                                                                                    |\n";
+    std::cout <<"|                                                                                                    |\n";
+    std::cout <<"+====================================================================================================+\n";
+    std::cout <<"\n";
+}
+
 int main()
 {
     system("cls");
     dateTime();
     gpuSummary();
+    displayProcess();
     return 0; 
 }
